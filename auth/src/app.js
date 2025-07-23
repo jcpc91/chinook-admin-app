7274ee653def28332416179506b8fb7bd10937ba
jcpc91 <jcpc91@hotmail.com>
2025-07-23 00:33:43 +0000

    inmerory auth

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-import UserRepository from "./repositories/user.repository.js";
+const UserRepository = require("./repositories/inMemoryRepository/user.repository.js");
 const simulateLongIOProcess = require("./simulateLongCPUProcess");
 require("dotenv").config();
 console.log("env: ", process.env);
@@ -9,38 +9,37 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const userRepository = new UserRepository();
-userRepository.add({ id: 1, username: "admin", password: "admin" });
 
 // Enable CORS
 app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  }),
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
 );
 
 // Enable Express to parse JSON request bodies
 app.use(express.json());
 
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
 app.get("/", (req, res) => {
-  res.send("hola mundo auth");
+    res.send("hola mundo auth");
 });
 // Login route
 app.post("/", async (req, res) => {
-  const { username, password } = req.body;
-  const user = userRepository.getAll().find(u => u.username === username && u.password === password);
+    const { username, password } = req.body;
+    const user = userRepository.validUserPassword(username, password);
 
-  if (user) {
-    // Generate JWT token
-    const token = jwt.sign({ user: user.username }, process.env.JWT_SECREAT_KEY, {
-      expiresIn: "2h",
-    });
-    res.json({ token });
-  } else {
-    // Invalid credentials
-    res.status(401).json({ message: "Invalid credentials" });
-  }
+    if (user) {
+        // Generate JWT token
+        const token = jwt.sign({ user: user.username }, process.env.JWT_SECREAT_KEY, {
+            expiresIn: "2h",
+        });
+        res.json({ token });
+    } else {
+        // Invalid credentials
+        res.status(401).json({ message: "Invalid credentials" });
+    }
 });
---
+++ auth/src/repositories/inMemoryRepository/user.repository.js
@@ -0,0 +1,42 @@
+const IBaseRepository = require("../IBaseRepository");
+
+module.exports = class UserRepository extends IBaseRepository {
+    constructor() {
+        super();
+        this.users = [{ id: 1, username: "admin", password: "admin" }];
+    }
+
+    getAll() {
+        return this.users;
+    }
+
+    get(id) {
+        return this.users.find((user) => user.id === id);
+    }
+
+    add(user) {
+        this.users.push(user);
+    }
+
+    update(id, updatedUser) {
+        const userIndex = this.users.findIndex((user) => user.id === id);
+        if (userIndex === -1) {
+            return null;
+        }
+        this.users[userIndex] = { ...this.users[userIndex], ...updatedUser };
+        return this.users[userIndex];
+    }
+
+    delete(id) {
+        const userIndex = this.users.findIndex((user) => user.id === id);
+        if (userIndex === -1) {
+            return false;
+        }
+        this.users.splice(userIndex, 1);
+        return true;
+    }
+
+    validUserPassword(username, password) {
+        return this.users.find((u) => u.username === username && u.password === password);
+    }
+};
